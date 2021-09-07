068a784ce4507bb0033174614ae87693b80490c1
Fernando Maclen <fmaclen@gmail.com>
2021-09-06 18:50:51 -0700

    Add unique id to transaction descriptions in the test CSV factory (#182)

--- src/__tests__/factories/mintCsvEntryFactory.ts
+++ src/__tests__/factories/mintCsvEntryFactory.ts
@@ -1,19 +1,21 @@
 import { build, fake, oneOf } from '@jackfranklin/test-data-bot';
 import { format } from 'date-fns';
 
+export const uniqueDescription = () => {
+  const uniqueId = new Date().getTime().toString().slice(-4);
+  return `${fake(f => f.company.companyName())} ${uniqueId}`;
+};
+
 export const mintCSVEntryBuilder = build('Mint CSV Entry', {
   fields: {
-    Date: format(
-      new Date(),
-      'M/dd/yyyy'
-    ),
-    'Original Description': fake(f => f.lorem.sentence()),
+    Date: format(new Date(), 'M/dd/yyyy'),
+    'Original Description': uniqueDescription(),
     'Transaction Type': oneOf(['credit', 'debit']),
-    Description: fake(f => f.lorem.sentence()),
-    Amount: fake(f => f.finance.amount(0)),
+    Description: uniqueDescription(),
+    Amount: fake(f => f.finance.amount()),
     Category: fake(f => f.finance.transactionType()),
     'Account Name': fake(f => f.finance.accountName()),
     Labels: fake(f => f.lorem.sentence()),
-    Notes: fake(f => f.lorem.sentence())
+    Notes: fake(f => f.lorem.sentence()),
   },
 });
--- src/__tests__/unit/mintImport.spec.ts
+++ src/__tests__/unit/mintImport.spec.ts
@@ -2,16 +2,19 @@ import { mintCSVEntryBuilder } from '../factories/mintCsvEntryFactory';
 import { mintCsvToJson } from '../../electron/helpers/sourceHelpers/mint';
 
 test('Mint CSV to Json', () => {
-  const personalCapitalCSVEntries = [...Array(20).fill(mintCSVEntryBuilder()), ...Array(30).fill(mintCSVEntryBuilder())];
-  expect(mintCsvToJson(personalCapitalCSVEntries)).not.toBeNull();
+  const mintCSVEntries = [
+    ...Array(20).fill(mintCSVEntryBuilder()),
+    ...Array(30).fill(mintCSVEntryBuilder()),
+  ];
+  expect(mintCsvToJson(mintCSVEntries)).not.toBeNull();
 
   // Metadata
-  expect(mintCsvToJson(personalCapitalCSVEntries).metadata.countAccounts).toBe(2);
-  expect(mintCsvToJson(personalCapitalCSVEntries).metadata.countTransactions).toBe(50);
+  expect(mintCsvToJson(mintCSVEntries).metadata.countAccounts).toBe(2);
+  expect(mintCsvToJson(mintCSVEntries).metadata.countTransactions).toBe(50);
 
   // Canutin File
-  expect(mintCsvToJson(personalCapitalCSVEntries).data.accounts.length).toBe(2);
-  expect(mintCsvToJson(personalCapitalCSVEntries).data.assets).toBeUndefined();
-  expect(mintCsvToJson(personalCapitalCSVEntries).data.accounts[0].transactions.length).toBe(20);
-  expect(mintCsvToJson(personalCapitalCSVEntries).data.accounts[1].transactions.length).toBe(30);
+  expect(mintCsvToJson(mintCSVEntries).data.accounts.length).toBe(2);
+  expect(mintCsvToJson(mintCSVEntries).data.assets).toBeUndefined();
+  expect(mintCsvToJson(mintCSVEntries).data.accounts[0].transactions.length).toBe(20);
+  expect(mintCsvToJson(mintCSVEntries).data.accounts[1].transactions.length).toBe(30);
 });
